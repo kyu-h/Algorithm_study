0b39748e93f3c1c6305f2ef5a4822e74c37da993
kyu-h <tigerk9212@gmail.com>
2017-10-09 15:20:10 +0900

    quick sort(Ascending, Descending) study due to 171010

---
+++ QuickSort/src/Descending_sort.java
@@ -0,0 +1,30 @@
+public class Descending_sort {
+    public void sort(int[] data, int x, int y){
+        int left = x;
+        int right = y;
+        int pivot = data[(x+y)/2];
+
+        do{
+            while(data[left] > pivot){
+                left++;
+            }
+            while(data[right] < pivot){
+                right--;
+            }
+            if(left <= right){
+                int temp = data[left];
+                data[left] = data[right];
+                data[right] = temp;
+                left++;
+                right--;
+            }
+        }while (left <= right);
+
+        if(x < right){
+            sort(data, x, right);
+        }
+        if(y > left){
+            sort(data, left, y);
+        }
+    }
+}
--- QuickSort/src/Quick.java
+++ QuickSort/src/Quick.java
@@ -3,39 +3,19 @@ public class Quick {
     public static void main(String[] args) {
         int data[] = {66, 0, -71, 34, 5, -10, 500, 975};
 
-        Quick quick = new Quick();
-        quick.sort(data, 0, data.length - 1);
+        Descending_sort descendingSort = new Descending_sort();
+        descendingSort.sort(data, 0, data.length - 1);
+        System.out.println("내림차순");
         for(int i=0; i<data.length; i++){
-            System.out.println(i + "번째 = " + data[i]);
+            System.out.print(i + " = " + data[i] + " | ");
         }
-    }
+        System.out.println();
 
-    public void sort(int[] data, int x, int y){
-        int left = x;
-        int right = y;
-        int pivot = data[(x+y)/2];
-
-        do{
-            while(data[left] < pivot){
-                left++;
-            }
-            while(data[right] > pivot){
-                right--;
-            }
-            if(left <= right){
-                int temp = data[left];
-                data[left] = data[right];
-                data[right] = temp;
-                left++;
-                right--;
-            }
-        }while (left <= right);
-
-        if(x < right){
-            sort(data, x, right);
-        }
-        if(y > left){
-            sort(data, left, y);
+        Ascending_sort ascendingSort = new Ascending_sort();
+        ascendingSort.sort(data, 0, data.length - 1);
+        System.out.println("오름차순");
+        for(int i=0; i<data.length; i++){
+            System.out.print(i + " = " + data[i] + " | ");
         }
     }
 }
