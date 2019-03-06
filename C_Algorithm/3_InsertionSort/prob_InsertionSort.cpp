cc17c5651f17943bb177e6b25b0225a63e65aef0
kyuh <tigerk9212@gmail.com>
2019-03-06 23:23:15 +0900

    example of insertion sort

--- C_Algorithm/3_InsertionSort/prob_InsertionSort.cpp
+++ C_Algorithm/3_InsertionSort/prob_InsertionSort.cpp
@@ -6,13 +6,13 @@ int main(void){
 	
 	scanf("%d", &size);
 	
-	for(i=0; i<10; i++){
+	for(i=0; i<size; i++){
 		scanf("%d", &arr[i]);
 	}
 	
 	for(i=0; i<size; i++){
 		j = i;
-		while(arr[j-1] > arr[j] && j > 0){
+		while(arr[j-1] < arr[j] && j > 0){ // 등호만 바꾸면 오름 내림 바꿀 수 있음 
 			tmp = arr[j-1];
 			arr[j-1] = arr[j];
 			arr[j] = tmp;
