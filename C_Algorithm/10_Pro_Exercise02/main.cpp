cbf83a5b8583999b072fee9b0952d9e1a3abee3d
kyuh <tigerk9212@gmail.com>
2019-03-07 20:33:46 +0900

    exercise 10989 using CountingSort
    https://www.acmicpc.net/problem/10989

---
+++ C_Algorithm/10_Pro_Exercise02/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int a[10001];
+
+int main(){
+    scanf("%d", &n);
+
+    for(int i=0; i<n; i++){
+        int x;
+        scanf("%d", &x);
+        a[x]++;
+    }
+    for(int i=0; i<10001; i++){
+        while(a[i] != 0){
+            printf("%d\n", i);
+            a[i]--;
+        }
+    }
+    return 0;
+}
